13b903c8675e81c245936fd2eec561124dec3659
michelletaing <mleetaing@gmail.com>
2023-01-28 13:49:27 -0500

    set up basic structure and added bg image

--- hello.py
+++ hello.py
@@ -1,18 +1,42 @@
 import pygame
+from pygame.locals import *
 
-#initialising pygame
 pygame.init()
 
-#defining size of game window
-windowsSize = pygame.display.set_mode((800,600)) 
-pygame.display.set_caption("Hello World Printer")
+# Set dimensions
+width, height = 1280, 720
+window = pygame.display.set_mode((width, height))
 
-#defining font attributes
-myFont = pygame.font.SysFont("Segoe UI", 90)
-helloWorld = myFont.render("Hello Worldo", 1, (255, 0, 255), (255, 255, 255))
+# Set images
+thumbnail_nick = pygame.image.load('assets/thumbnail_nick.png').convert()
+thumbnail_nick = pygame.transform.scale(thumbnail_nick, (width,height))
 
-while 1:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT: sys.exit()
-    windowsSize.blit(helloWorld, (0, 0))
+test_obj = pygame.image.load('assets/drawer.png').convert()
+test_obj = pygame.transform.scale(test_obj, (163,150))
+test_rect = test_obj.get_rect()
+
+def draw_window():
+    window.blit(thumbnail_nick, (0, 0))
+    window.blit(test_obj, (293, 405))
     pygame.display.update()
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                run = False
+
+        mouse_pos = pygame.mouse.get_pos()
+        
+        if pygame.mouse.get_pressed()[0]:
+            print(mouse_pos)
+            if (test_rect.collidepoint(mouse_pos)):
+                print("bruh")
+
+        draw_window()
+        
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
