4528be8f9d6f01cfc46326e82512597d868e202a
michelletaing <mleetaing@gmail.com>
2023-01-28 22:43:54 -0500

    attempt to switch between screens

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from pygame.locals import *
 
 pygame.init()
@@ -9,36 +10,111 @@ window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("nick's apartment")
 
 # SET IMAGES
-bg = pygame.image.load('assets/BACKGROUND.png').convert_alpha()
-air = pygame.image.load('assets/air.png').convert_alpha()
-cabinet = pygame.image.load('assets/cabinet.png').convert_alpha()
-couch = pygame.image.load('assets/couch.png').convert_alpha()
-kitchen = pygame.image.load('assets/kitchen.png').convert_alpha()
-lamp = pygame.image.load('assets/lamp.png').convert_alpha()
-poster = pygame.image.load('assets/poster.png').convert_alpha()
-small_table = pygame.image.load('assets/small_table.png').convert_alpha()
-table = pygame.image.load('assets/table.png').convert_alpha()
-trash = pygame.image.load('assets/trash.png').convert_alpha()
-window_drawing = pygame.image.load('assets/window.png').convert_alpha()
-gradient = pygame.image.load('assets/gradient.png').convert_alpha()
+# arrows
+arrow_left = pygame.image.load(f'assets/arrow_left.png').convert_alpha()
+left_rect = arrow_left.get_rect()
+left_rect.topleft = (83,87)
 
-# SET RECT BOUNDS
-air_rect = air.get_rect();
-air_rect.topleft = (10, 517)
+arrow_right = pygame.image.load(f'assets/arrow_right.png').convert_alpha()
+right_rect = arrow_right.get_rect()
+right_rect.topleft = (1089, 343)
+
+# initialize views lists and load tv_view images
+tv_view = []
+for i in range(1,11):
+    tv_view.append(pygame.image.load(f'assets/tv_view/{i}.png').convert_alpha())
+
+couch_view = []
+#or i in range(1,13):
+    #couch_view.append(pygame.image.load(f'assets/couch_view/{i}.png').convert_alpha()) 
+
+window_view = []
+#for i in range(1,6):
+    #window_view.append(pygame.image.load(f'assets/window_view/{i}.png').convert_alpha()) 
+
+kitchen_view = []
+#for i in range(1,10):
+    #couch_view.append(pygame.image.load(f'assets/kitchen_view/{i}.png').convert_alpha()) 
+
+def toggle_view(images, turnOn):
+    window.fill((0,0,0))
+    if turnOn:
+        for i in range(len(images)):
+            if images[i] is not None:
+                window.blit(images[i], (0,0))
+    if not turnOn:
+        for i in range(len(images)):
+            images[i] = None       
+
+def switch_views(event):
+    if window_view is not None:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
+            if left_rect.collidepoint(mouse_pos):
+                toggle_view(window_view, False)
+                # Reload the images
+                for i in range(1,11):
+                    tv_view.append(pygame.image.load(f'assets/tv_view/{i}.png').convert_alpha()) 
+                toggle_view(tv_view, True)
+            if right_rect.collidepoint(mouse_pos):
+                toggle_view(window_view, False)
+                for i in range(1,13):
+                    couch_view.append(pygame.image.load(f'assets/couch_view/{i}.png').convert_alpha()) 
+                toggle_view(couch_view, True)
+    
+    if tv_view is not None:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
+            if left_rect.collidepoint(mouse_pos):
+                toggle_view(tv_view, False)
+                for i in range(1,10):
+                    couch_view.append(pygame.image.load(f'assets/kitchen_view/{i}.png').convert_alpha()) 
+                toggle_view(kitchen_view, True)
+            if right_rect.collidepoint(mouse_pos):
+                toggle_view(tv_view, False)
+                for i in range(1,6):
+                    window_view.append(pygame.image.load(f'assets/window_view/{i}.png').convert_alpha()) 
+                toggle_view(window_view, True)
+    
+    if couch_view is not None:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
+            if left_rect.collidepoint(mouse_pos):
+                toggle_view(couch_view, False)
+                for i in range(1,6):
+                    window_view.append(pygame.image.load(f'assets/window_view/{i}.png').convert_alpha()) 
+                toggle_view(window_view, True)
+            if right_rect.collidepoint(mouse_pos):
+                toggle_view(couch_view, False)
+                for i in range(1,10):
+                    couch_view.append(pygame.image.load(f'assets/kitchen_view/{i}.png').convert_alpha()) 
+                toggle_view(kitchen_view, True)
+
+
+    if kitchen_view is not None:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
+            if left_rect.collidepoint(mouse_pos):
+                toggle_view(kitchen_view, False)
+                for i in range(1,13):
+                    couch_view.append(pygame.image.load(f'assets/couch_view/{i}.png').convert_alpha()) 
+                toggle_view(couch_view, True)
+            elif right_rect.collidepoint(mouse_pos):
+                toggle_view(kitchen_view, False)
+                for i in range(1,11):
+                    tv_view.append(pygame.image.load(f'assets/tv_view/{i}.png').convert_alpha()) 
+                toggle_view(tv_view, True)
+    
+    pygame.display.flip()
+
 
 def render():
-    window.blit(bg, (0,0))
-    window.blit(air, (0,0))
-    window.blit(cabinet, (0,0))
-    window.blit(couch, (0,0))
-    window.blit(kitchen, (0,0))
-    window.blit(lamp, (0,0))
-    window.blit(poster, (0,0))
-    window.blit(small_table, (0,0))
-    window.blit(table, (0,0))
-    window.blit(trash, (0,0))
-    window.blit(window_drawing, (0,0))
-    window.blit(gradient, (0,0))
+    for image in tv_view:
+        if image is not None:
+            window.blit(image, (0,0))
+    
+    window.blit(arrow_left, (0,0))
+    window.blit(arrow_right, (0,0))
     pygame.display.update()
 
 def main():
@@ -47,14 +123,11 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT:
                 run = False
-
-        if pygame.mouse.get_pressed()[0]:
-            mouse_pos = pygame.mouse.get_pos()
-            if (air_rect.collidepoint(mouse_pos)):
-                print("bruh")
+            
+            switch_views(event)
 
         render()
-        
+
     pygame.quit()
 
 if __name__ == "__main__":
